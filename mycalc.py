1226eb9a3208222e26224494f21f3316b3daf0bc
Mukhtar Raji <oladuaraji@gmail.com>
2021-06-11 12:27:55 +0100

    Update

---
+++ mycalc.py
@@ -0,0 +1,20 @@
+import random
+
+class MyCalc():
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def mod_divide(self):
+        if self.b == 0:
+            raise ValueError('Cannot divide by zero')
+
+        return (int(self.a//self.b), self.a%self.b)
+
+    def rand_between(self):
+        return( (random.random() * abs(self.a - self.b)) +
+                min(self.a, self.b) )
